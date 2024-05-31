c08bb52ea484eb7149e2768b93044404520f5616
jickpa17 <jickpa17@hotmail.com>
2024-05-31 17:19:01 -0500

    Se crea otro componente "servicios"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { ProductosComponent } from './productos/productos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 export const routes: Routes = [
     {path:'',component:InicioComponent},
     {path:'inicio',component:InicioComponent},
     {path:'nosotros',component:NosotrosComponent},
-    {path:'productos',component:ProductosComponent}
+    {path:'productos',component:ProductosComponent},
+    {path:'servicios',component:ServiciosComponent}
 ];
